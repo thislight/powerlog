1ca2bd650ace7bcb268b35a8a9bd0dbaacda4e46
thisLight <l1589002388@gmail.com>
2020-09-19 12:00:03 +0800

    topstring: output correct table list

--- powerlog/topstring.lua
+++ powerlog/topstring.lua
@@ -1,6 +1,11 @@
 local function topstring(t)
     if type(t) == 'table' then
         local buffer =  {}
+        if #t > 0 then
+            for i, v in ipairs(t) do
+                table.insert(buffer, topstring(v))
+            end
+        end
         for k,v in pairs(t) do
             table.insert(buffer, string.format("%s=%s", topstring(k), topstring(v)))
         end
